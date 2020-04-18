e3cbe24b874494e9025031cdd1cef934790af4d3
tranhailong <hailong.tran@thanglong.co.uk>
2020-04-18 14:26:49 +0800

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ConsoleComponent } from './console/console.component';
+import { ListingComponent } from './listing/listing.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ConsoleComponent, ],
+  declarations: [ AppComponent, ConsoleComponent, ListingComponent, ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
