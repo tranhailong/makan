3d83704df2f28dc54aac4d57090ba5f55c8b8238
tranhailong <hailong.tran@thanglong.co.uk>
2020-04-18 14:17:26 +0800

    5 files modified, 2 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'makan-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
 }
